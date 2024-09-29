0b711d8d669880e1bbf11e2acb5008b2ed176833
Connor Hainje <cmhainje@gmail.com>
2024-09-29 19:52:45 -0400

    add rcParams

--- chic/__init__.py
+++ chic/__init__.py
@@ -3,5 +3,9 @@ from .schemes import (
     set_default,
     make_colormap_from_scheme
 )
+from .defaults import params
 
+import matplotlib as mpl
+
+mpl.rcParams.update(params)
 set_default('tol_bright')
---
+++ chic/defaults.py
@@ -0,0 +1,22 @@
+params = {
+    'text.usetex': True,
+    'text.latex.preamble': r"""
+        \usepackage{amsmath}
+    """,
+    'font.family': 'sans-serif',
+    'figure.dpi':  144,
+    'font.size': 12,
+    'axes.labelsize': 12,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'figure.figsize': (5, 4),
+    'xtick.bottom': True,
+    'xtick.top': True,
+    'xtick.minor.visible': True,
+    'xtick.direction': 'in',
+    'ytick.left': True,
+    'ytick.right': True,
+    'ytick.minor.visible': True,
+    'ytick.direction': 'in',
+    'image.cmap': 'tol_iridescent_r',
+}
